acd55b0aadd9fe379a65f0c8fad7f06fada42e29
Boat <ratna.tridentverse@gmail.com>
2024-10-29 17:49:54 +0530

    Added lobby to gamelevel

--- Source/BoatRace/Private/GameMode/LobbyGameMode.cpp
+++ Source/BoatRace/Private/GameMode/LobbyGameMode.cpp
@@ -3,6 +3,7 @@
 
 #include "GameMode/LobbyGameMode.h"
 #include "GameFramework/PlayerState.h"
+#include "GameFramework/GameState.h"
 
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
@@ -16,6 +17,18 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 		{
 			FString PlayerName = PlayerState->GetPlayerName();
 			GEngine->AddOnScreenDebugMessage(-1, 60.f, FColor::Green, *PlayerName);
+		}
+	}
+
+	int32 NumOfPlayers = GameState.Get()->PlayerArray.Num();
+
+	if (NumOfPlayers == 2)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Levels/GameLevel_1?listen"));
 		}
 	}
 }
